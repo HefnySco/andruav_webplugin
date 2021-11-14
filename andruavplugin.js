2f946b754549082558bd1e59e54c0c09fd4da38c
HefnySco <mohammad.hefny@gmail.com>
2021-11-15 00:13:40 +0200

    adding colors

--- andruavplugin.js
+++ andruavplugin.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 'use strict';
+let colors = require('./colors.js');
 let webSocket = require ('./websocket.js')
 let udp = require ('./udpclient.js')
 let program = require('commander');
@@ -13,12 +14,13 @@ program
   .option('-p --udp_target_port <port number>', 'Mission Planner UDP Port', 14550)
   .option('-i --udp_target_ip <ip address>', 'Mission Planner IP - default is broadcast to all', '255.255.255.255')
   .parse(process.argv);
-
-console.log ('Welcome to Andruav Web-Connector Telemetry version ' + pjson.version );
-console.log('Mission Planner is at:' + program.udp_target_ip + ':' + program.udp_target_port);
+  
+console.log (colors.colors.Bright + colors.colors.FgGreen +  'Welcome to ' +  colors.colors.FgYellow  + 'Andruav Web-Connector Telemetry' + colors.colors.FgGreen + ' version ' + colors.colors.FgYellow + pjson.version + colors.colors.Reset);
+console.log(colors.colors.Bright + colors.colors.FgGreen +  'Mission Planner is at: ' + colors.colors.FgYellow + program.udp_target_ip + ':' + program.udp_target_port + colors.colors.Reset);
 udp.startServer ("0.0.0.0","0",program.udp_target_port, program.udp_target_ip);
 webSocket.connect("127.0.0.1",8811);
-console.log ('PLEASE USE DRONEKIT CONNECTION');
+console.log (colors.colors.Bright + colors.colors.FgRed +  'PLEASE MAKE SURE TELEMETRY ON'  + colors.colors.Reset);
+console.log (colors.colors.Bright + "Check this video https://youtu.be/mUCbhzvmVcI for more info"  + colors.colors.Reset);
 
 webSocket.onMessageReceived = function (message)
 {
